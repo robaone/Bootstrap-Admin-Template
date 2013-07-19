85978d6d571fd446d9b159b9163dfe8125d35981
Ansel Robateau <robaone@gmail.com>
2013-07-19 07:25:20 -0500

    Incomplete - task : Create the user interface https://plus.google.com/116738584284283239999/posts/Jdr46b6nSLn

---
+++ MicroMonitor/src/com/microdg/gwt/monitor/client/view/SitesTable.java
@@ -0,0 +1,59 @@
+package com.microdg.gwt.monitor.client.view;
+
+import com.google.gwt.core.client.JavaScriptObject;
+import com.google.gwt.user.client.ui.Widget;
+import com.robaone.gwt.eventbus.client.EventDrivenComposite;
+import com.robaone.gwt.eventbus.client.widget.DynamicTableWidget;
+
+/**
+ * <pre>   Copyright 2013 Ansel Robateau
+         http://www.robaone.com
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+     http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.</pre>
+ * @author Ansel
+ *
+ */
+public class SitesTable extends EventDrivenComposite {
+	public SitesTable(){
+		bind();
+		DynamicTableWidget table = new DynamicTableWidget();
+		table.setStyleName("table");
+		this.initWidget(table);
+		String[] headers = {"Site Name","Action"};
+		table.setHeaders(headers);
+	}
+	@Override
+	public void handleEvent(String command, Widget message) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void handleEvent(JavaScriptObject message) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void handleEvent(String command, Widget[] messages) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void handleObjectEvent(Object message) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
