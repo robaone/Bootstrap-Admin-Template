429fc3fc2ae3b7948aa4b177da10713ea4a7754a
Ansel Robateau <robaone@gmail.com>
2013-08-20 07:49:00 -0500

    Incomplete - task : Create user interface for mass exposure application Adding error objects and handling.

--- MicroMonitor/src/com/microdg/gwt/monitor/client/view/login/LoginUi.java
+++ MicroMonitor/src/com/microdg/gwt/monitor/client/view/login/LoginUi.java
@@ -1,20 +1,26 @@
 package com.microdg.gwt.monitor.client.view.login;
 
 import com.google.gwt.core.client.GWT;
+import com.google.gwt.core.client.JavaScriptObject;
 import com.google.gwt.event.dom.client.ClickEvent;
 import com.google.gwt.uibinder.client.UiBinder;
 import com.google.gwt.uibinder.client.UiField;
 import com.google.gwt.uibinder.client.UiHandler;
 import com.google.gwt.user.client.ui.Button;
 import com.google.gwt.user.client.ui.Composite;
+import com.google.gwt.user.client.ui.HTMLPanel;
+import com.google.gwt.user.client.ui.InlineLabel;
 import com.google.gwt.user.client.ui.PasswordTextBox;
 import com.google.gwt.user.client.ui.TextBox;
 import com.google.gwt.user.client.ui.Widget;
+import com.microdg.gwt.monitor.shared.dto.FieldError;
+import com.microdg.gwt.monitor.shared.dto.GeneralError;
 import com.microdg.gwt.monitor.shared.dto.LoginInfoDTO;
 import com.robaone.gwt.eventbus.client.EventBus;
+import com.robaone.gwt.eventbus.client.EventDrivenComposite;
 import com.robaone.gwt.eventbus.client.ObjectChannelEvent;
 
-public class LoginUi extends Composite {
+public class LoginUi extends EventDrivenComposite {
 
 	private static LoginUiUiBinder uiBinder = GWT.create(LoginUiUiBinder.class);
 
@@ -42,6 +48,13 @@ public class LoginUi extends Composite {
 	@UiField TextBox registerEmail;
 	@UiField PasswordTextBox registerPassword;
 	@UiField Button registerSubmit;
+	@UiField HTMLPanel loginError;
+	@UiField HTMLPanel generalError;
+	@UiField HTMLPanel usernameError;
+	@UiField HTMLPanel passwordError;
+	@UiField InlineLabel generalErrorText;
+	@UiField InlineLabel usernameErrorText;
+	@UiField InlineLabel passwordErrorText;
 	
 	@UiHandler("submit")
 	void onClick(ClickEvent e){
@@ -77,5 +90,47 @@ public class LoginUi extends Composite {
 		
 		ObjectChannelEvent event = new ObjectChannelEvent("register-user",logininfo); 
 		EventBus.handleObjectEvent(event);
+	}
+
+	@Override
+	public void handleEvent(String command, Widget message) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void handleEvent(JavaScriptObject message) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void handleEvent(String command, Widget[] messages) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void handleObjectEvent(Object message) {
+		if(message instanceof GeneralError){
+			handleError((GeneralError)message);
+		}else if(message instanceof FieldError){
+			handleError((FieldError)message);
+		}
+	}
+
+	private void handleError(FieldError message) {
+		if("username".equalsIgnoreCase(message.getFieldName())){
+			this.usernameError.setVisible(true);
+			this.usernameErrorText.setText(message.getError());
+		}else if("password".equalsIgnoreCase(message.getFieldName())){
+			this.passwordError.setVisible(true);
+			this.passwordErrorText.setText(message.getError());
+		}
+	}
+
+	private void handleError(GeneralError error) {
+		this.generalError.setVisible(true);
+		this.generalErrorText.setText(error.getError());
 	}
 }
---
+++ MicroMonitor/src/com/microdg/gwt/monitor/shared/dto/GeneralError.java
@@ -0,0 +1,10 @@
+package com.microdg.gwt.monitor.shared.dto;
+
+public class GeneralError {
+
+	public String getError() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
